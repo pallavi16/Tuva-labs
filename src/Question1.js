ff903f82c847fc287e6ec69609972a3cb80dbe43
Pallavi Aggarwal <pallaviaggarwal@Satishs-MacBook-Pro.local>
2018-11-30 17:09:33 -0700

    Initial commit

--- src/Question1.js
+++ src/Question1.js
@@ -8,44 +8,33 @@ class Question1 extends Component {
 
         <h2 className="fadeInText">QUESTION 1: <br/>
 
-     Tell us about yourself, and your expectations from a new job.
-Do address the following in your response:
-<ul>
-<li>A short introduction about yourself.</li>
-<li>Your reasons for seeking a new opportunity, and your expectations from the same.</li>
-<li>Your most important considerations while evaluating an opportunity.</li>
-<li>Your present compensation, and your expectations from the new job.</li>
-</ul>
+          What new technology-related knowledge or skills (new language, new tools, etc.)
+          have you acquired in the past 12 months? Anything excites you specifically?
+
 </h2>
         <p className ="blurText">
-        <strong>Current Job?</strong> I am currently working as a software design engineer at <b> <a href="https://healthcare.utah.edu/huntsmancancerinstitute/"> Huntsman Cancer Institute </a> </b>writing a web application ‘ART Admin’ using React. I recently finished the migration of a project called ‘Research Administrator’ (working with the frontend team) from AngularJS to Angular5.
-        <br/>Web Stack and Tools Used: AngularJS, Angular 5, React, IntelliJ IDEA, Git etc. <br/>
-        <br/>
-        <strong>Previous Job?</strong> Previously I worked as a software developer with a stealth startup called <b><a href="https://www.ebibelot.com/"> Ebibelot  </a></b>based in San Jose, CA. I was working there to build a musical application demo for kids on web and mobile platform using AR/VR and AI technology. The goal was to try and leverage AI technology in the educational space and build a platform to revolutionize the use of camera.
-        <br/><br/>
-        <strong>Education?</strong> I graduated with a Masters in Comp Engineering  from the <b><a href="http://www.cs.utah.edu/">University of Utah </a></b>with relevant coursework in Algorithms, Database Systems, Artificial Intelligence and Operating systems.
-        <br/><br/>
-        <strong>Internship?</strong> Worked as a Web Developer Intern with ‘Swisk’ in New York to develop web and mobile vehicle seeking application similar to Uber. My task was to implement customer and driver side RESTful APIs in Java.
-        <br/><br/>
-        <strong>Fun Projects?</strong> I believe that building projects is the best way to learn, not just the language but also learning to fix errors. So, I usually like to explore new technologies by building my own web applications. Some of the cool stuff can be found
-        <b><a href="https://github.com/pallavi16?tab=repositories"> here: </a></b>
+          I started working as a software design engineer at the <b><a
+          href="https://healthcare.utah.edu/huntsmancancerinstitute/">
+          Huntsman Cancer Institute </a></b>, Utah,
+          United States about 11 months ago and I believe that my technical stack has advanced
+          tremendously since then. I worked on some major projects here involving JavaScript
+          frameworks ( AngularJS, Angular5, React ), Programming Language (Java) , Server(WildFly, Orion),
+           Tools (Git, Webpack, Babel), IDE(Eclipse EE, IntelliJ IDEA), Styling (CSS3, SASS).
+    I worked on migrating an existing codebase written in AngularJS to Angular5. I was new to
+    Angular and React when I initially joined but after building a few demo projects, I acquired a
+     firm grip over JavaScript frameworks within two months. It was really exciting to work with
+     a diverse team of incredibly talented engineers, developers and managers and learn new languages.
 
-        <br/><br/>
-        <strong>Why new job?</strong> Personal reason: Willing to relocate to India (close to family); <br/>Professional Reason:  find work that is more challenging than my current job. <br/> Currently, I’m looking for something that could put wheels on my career to learn and advance my skills as a developer.
-        <br/><br/>
-        <strong>Considerations for evaluating an opportunity or expectations from the new job?</strong>
-        <ul >
-        <li>A place where my work is not limited to web development. If need be, I could help and switch to full stack and learn something new in the process.</li>
-        <li>A place where there is an opportunity to work in an agile environment with a possibility to collaborate with the rest of the team.</li>
-        <li>A team whose goal is to conceptualize ideas to build next generation of pixel-perfect web interfaces/designs and create amazing user experiences.</li>
-        <li>A work culture where each step/stage (Plan, Design, Build, Test, Launch) is given its own importance.</li>
-        <li> A place where work-life balance is considered important.</li>
-        <li>(Optional but not mandatory) Considering the long commute time in Delhi and to ensure time management, possibility of working remote sometimes.</li>
-        </ul>
-
-        <strong>Present compensation?</strong> 70,000 USD<br/>
-        <strong>Future expected compensation? </strong>The range mentioned in the job description should be good to start with!
-        <br/><br/>
+    Alongside, I recently completed the <b><a href="https://grow.google/programs/udacity/">Google Scholarship program</a></b> as a Mobile Specialist
+    where I got an opportunity to work on a huge project called ‘Restaurant Review’ in three
+    stages for 10 months. I learnt the concept of service workers, building a fully responsive
+    mobile-ready offline-first web application, implemented various accessibility features to
+     improve performance and speed (page load time). The link to the project can be found
+     <b><a href="https://github.com/pallavi16/Restaurant-Review-Stage3">here.</a></b>
+     The project was really interesting and was a great resource to hone my
+     technical web development skills. I believe I have become much better,
+      self-driven and a more passionate front-end engineer in the past 12 months.
+  <br/><br/>
 
 
         </p>
